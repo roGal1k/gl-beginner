1b6f9ced73b0591eedb4d97cda39c59661a075eb
roGal1k <80055564+roGal1k@users.noreply.github.com>
2023-10-20 20:30:45 +0300

    add second task

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <windows.h>
 #include <gl/gl.h>
+#include <math.h>
+#include <stdio.h>
+
+#define M_PI 3.1415926535
 
 LRESULT CALLBACK WindowProc(HWND, UINT, WPARAM, LPARAM);
 void EnableOpenGL(HWND hwnd, HDC*, HGLRC*);
@@ -81,15 +85,6 @@ int WINAPI WinMain(HINSTANCE hInstance,
             glClear(GL_COLOR_BUFFER_BIT);
 
             glPushMatrix();
-            //glRotatef(theta, 0.0f, 0.0f, 1.0f);
-
-            //glBegin(GL_TRIANGLES);
-
-                //glColor3f(1.0f, 0.0f, 0.0f);   glVertex2f(0.0f,   1.0f);
-                //glColor3f(0.0f, 1.0f, 0.0f);   glVertex2f(0.87f,  -0.5f);
-                //glColor3f(0.0f, 0.0f, 1.0f);   glVertex2f(-0.87f, -0.5f);
-
-            //glEnd();
 
             glLineWidth(0.25f);
 
@@ -108,13 +103,13 @@ int WINAPI WinMain(HINSTANCE hInstance,
 
             glBegin(GL_TRIANGLES);
 
-                glColor3f(0.0f, 0.1f, 1.0f); glVertex2d(-1.0f, 1.0f);
-                glColor3f(0.0f, 0.1f, 1.0f); glVertex2d(-1.0f, 0.0f);
-                glColor3f(0.0f, 0.1f, 1.0f); glVertex2d(1.0f, 1.0f);
+                glColor3f(0.0f, 0.1f, 0.6f); glVertex2d(-1.0f, 1.0f);
+                glColor3f(0.0f, 0.1f, 0.6f); glVertex2d(-1.0f, 0.0f);
+                glColor3f(0.0f, 0.1f, 0.6f); glVertex2d(1.0f, 1.0f);
 
-                glColor3f(0.0f, 0.1f, 1.0f); glVertex2d(1.0f, 1.0f);
-                glColor3f(0.0f, 0.1f, 1.0f); glVertex2d(1.0f, 0.0f);
-                glColor3f(0.0f, 0.1f, 1.0f); glVertex2d(-1.0f, 0.0f);
+                glColor3f(0.0f, 0.1f, 0.6f); glVertex2d(1.0f, 1.0f);
+                glColor3f(0.0f, 0.1f, 0.6f); glVertex2d(1.0f, 0.0f);
+                glColor3f(0.0f, 0.1f, 0.6f); glVertex2d(-1.0f, 0.0f);
 
                 glColor3f(0.4f, 0.4f, 0.2f); glVertex2d(-0.4f, -0.4f);
                 glColor3f(0.4f, 0.4f, 0.2f); glVertex2d(-0.4f, 0.4f);
@@ -128,13 +123,119 @@ int WINAPI WinMain(HINSTANCE hInstance,
                 glColor3f(0.4f, 0.5f, 0.2f); glVertex2d(0.0f, 0.8f);
                 glColor3f(0.4f, 0.5f, 0.2f); glVertex2d(-0.4f, 0.4f);
 
-                glColor3f(0.0f, 0.1f, 1.0f); glVertex2d(-0.25f, 0.25f);
-                glColor3f(0.0f, 0.1f, 1.0f); glVertex2d(-0.25f, 0.0f);
-                glColor3f(0.0f, 0.1f, 1.0f); glVertex2d(1.0f, 1.0f);
 
-                glColor3f(0.0f, 0.1f, 1.0f); glVertex2d(1.0f, 1.0f);
-                glColor3f(0.0f, 0.1f, 1.0f); glVertex2d(1.0f, 0.0f);
-                glColor3f(0.0f, 0.1f, 1.0f); glVertex2d(-1.0f, 0.0f);
+                glColor3f(0.0f, 0.0f, 1.0f); glVertex2d(0.2f, 0.2f);
+                glColor3f(0.0f, 0.0f, 1.0f); glVertex2d(-0.2f, -0.2f);
+                glColor3f(0.0f, 0.0f, 1.0f); glVertex2d(0.2f, -0.2f);
+
+                glColor3f(0.0f, 0.0f, 1.0f); glVertex2d(-0.2f, 0.2f);
+                glColor3f(0.0f, 0.0f, 1.0f); glVertex2d(0.2f, 0.2f);
+                glColor3f(0.0f, 0.0f, 1.0f); glVertex2d(-0.2f, -0.2f);
+
+                // glColor3f(0.0f, 0.1f, 1.0f); glVertex2d(-0.25f, 0.25f);
+                // glColor3f(0.0f, 0.1f, 1.0f); glVertex2d(-0.25f, 0.0f);
+                // glColor3f(0.0f, 0.1f, 1.0f); glVertex2d(1.0f, 1.0f);
+
+                // glColor3f(0.0f, 0.1f, 1.0f); glVertex2d(1.0f, 1.0f);
+                // glColor3f(0.0f, 0.1f, 1.0f); glVertex2d(1.0f, 0.0f);
+                // glColor3f(0.0f, 0.1f, 1.0f); glVertex2d(-1.0f, 0.0f);
+
+                // body tree
+                glColor3f(0.65f, 0.2f, 0.0f); glVertex2d(0.8f, -0.4f);
+                glColor3f(0.65f, 0.2f, 0.0f); glVertex2d(0.8f, 0.2f);
+                glColor3f(0.65f, 0.2f, 0.0f); glVertex2d(0.9f, 0.2f);
+
+                glColor3f(0.65f, 0.2f, 0.0f); glVertex2d(0.9f, -0.4f);
+                glColor3f(0.65f, 0.2f, 0.0f); glVertex2d(0.9f, 0.2f);
+                glColor3f(0.65f, 0.2f, 0.0f); glVertex2d(0.8f, -0.4f);
+            glEnd();
+
+            glBegin(GL_LINES);
+
+                // roof triangle border
+                glColor3f(0.0f, 0.0f, 0.0f); glVertex2d(-0.4f, 0.4f);
+                glColor3f(0.0f, 0.0f, 0.0f); glVertex2d(0.0f, 0.8f);
+
+                glColor3f(0.0f, 0.0f, 0.0f); glVertex2d(0.4f, 0.4f);
+                glColor3f(0.0f, 0.0f, 0.0f); glVertex2d(0.0f, 0.8f);
+
+                glColor3f(0.0f, 0.0f, 0.0f); glVertex2d(-0.4f, 0.4f);
+                glColor3f(0.0f, 0.0f, 0.0f); glVertex2d(0.4f, 0.4f);
+
+                // body triangle border
+                glColor3f(0.0f, 0.0f, 0.0f); glVertex2d(0.0f, 0.2f);
+                glColor3f(0.0f, 0.0f, 0.0f); glVertex2d(0.0f, -0.2f);
+
+                glColor3f(0.0f, 0.0f, 0.0f); glVertex2d(-0.2f, 0.0f);
+                glColor3f(0.0f, 0.0f, 0.0f); glVertex2d(0.2f, 0.0f);
+
+                glColor3f(0.0f, 0.0f, 0.0f); glVertex2d(-0.2f, 0.2f);
+                glColor3f(0.0f, 0.0f, 0.0f); glVertex2d(0.2f, 0.2f);
+
+                glColor3f(0.0f, 0.0f, 0.0f); glVertex2d(-0.2f, -0.2f);
+                glColor3f(0.0f, 0.0f, 0.0f); glVertex2d(0.2f, -0.2f);
+
+                glColor3f(0.0f, 0.0f, 0.0f); glVertex2d(-0.2f, 0.2f);
+                glColor3f(0.0f, 0.0f, 0.0f); glVertex2d(-0.2f, -0.2f);
+
+                glColor3f(0.0f, 0.0f, 0.0f); glVertex2d(0.2f, 0.2f);
+                glColor3f(0.0f, 0.0f, 0.0f); glVertex2d(0.2f, -0.2f);
+
+                // window border
+                glColor3f(0.0f, 0.0f, 0.0f); glVertex2d(-0.4f, 0.4f);
+                glColor3f(0.0f, 0.0f, 0.0f); glVertex2d(0.4f, 0.4f);
+
+                glColor3f(0.0f, 0.0f, 0.0f); glVertex2d(-0.4f, -0.4f);
+                glColor3f(0.0f, 0.0f, 0.0f); glVertex2d(0.4f, -0.4f);
+
+                glColor3f(0.0f, 0.0f, 0.0f); glVertex2d(-0.4f, 0.4f);
+                glColor3f(0.0f, 0.0f, 0.0f); glVertex2d(-0.4f, -0.4f);
+
+                glColor3f(0.0f, 0.0f, 0.0f); glVertex2d(0.4f, 0.4f);
+                glColor3f(0.0f, 0.0f, 0.0f); glVertex2d(0.4f, -0.4f);
+
+            glEnd();
+
+            // sun
+            glBegin(GL_TRIANGLE_FAN);
+
+                float cnt = 20;
+                float r2 = 0.1;
+                float a = M_PI * 2 / cnt;
+                glColor3f(1.0f, 1.0f, 0.0f);
+                glVertex2d(0.8, 0.85);
+
+                for (int i=-1; i<cnt; i++)
+                {
+                    float x = 0.8 + (sin(a * i) * r2);
+                    float y = 0.8 + (cos(a * i) * r2);
+                    glVertex2d(x,y);
+                }
+
+            glEnd();
+
+            glBegin(GL_TRIANGLE_FAN);
+
+                r2 = 0.3;
+                glColor3f(0.0f, 1.0f, 0.0f);
+                glVertex2d(0.85, 0.35);
+                for (int i=-1; i<cnt; i++)
+                {
+                    float x = 0.85 + (sin(a * i) * r2);
+                    float y = 0.35 + (cos(a * i) * r2);
+                    glVertex2d(x,y);
+                }
+
+                r2 = 0.12;
+                glColor3f(0.0f, 1.0f, 0.0f);
+                glVertex2d(0.65, 0.15);
+                for (int i=-1; i<cnt; i++)
+                {
+                    float x = 0.65 + (sin(a * i) * r2);
+                    float y = 0.15 + (cos(a * i) * r2);
+                    glVertex2d(x,y);
+                }
+
             glEnd();
 
             glPopMatrix();
